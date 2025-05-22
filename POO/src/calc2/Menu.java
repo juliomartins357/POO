e3bc9f6cd1d3e8bf9e358dae1f678cfeede435fa
Viihh-77 <gabrielli_glowatski@estudante.sesisenai.org.br>
2025-05-21 21:50:58 -0300

    refacendo

--- POO/src/calc2/Menu.java
+++ POO/src/calc2/Menu.java
@@ -5,70 +5,128 @@ import java.util.Scanner;
 public class Menu {
     static Scanner sc = new Scanner(System.in);
 
-
     public static void main(String[] args) {
-
-
         int opcao;
         do {
-            System.out.println("Olá bem vindo  a calculadora;");
-            System.out.println("Qual  forma geométrica voê gostaria de calcular? ");
-            System.out.println("Opções \n 0.Sair  \n 1.Quadrado \n  2.Retângulo");
-
+            System.out.println("Bem vindo ao Sistema de Calculadora de Formas Geometricas");
+            System.out.println("Qual forma você deseja usar?");
+            System.out.println(
+                    "Opções: \n" +
+                            "0. Sair \n" +
+                            "1. Quadrado\n" +
+                            "2. Retângulo\n");
             opcao = sc.nextInt();
+            Formageometrica forma;
 
             switch (opcao) {
                 case 0: {
-                    System.out.println("Obrigdo  por utilizar o  nosso  sistema volte sempre!!!!!");
+                    System.out.println("Obrigado por usar o Sistema de Calculadora!");
                     break;
                 }
                 case 1: {
-                    System.out.println("Digite a cor do quadrado");
+                    System.out.println("Digite a cor do quadrado: ");
                     String cor = sc.next();
-                    System.out.println("digite o  lado  do quadrado");
+                    System.out.println("Digite o lado do quadrado: ");
                     double lado = sc.nextDouble();
 
-                    Quadrado quadrado = new Quadrado(cor, lado);
+                    forma = new Quadrado(cor, lado);
 
                     int opcao2;
-
                     do {
                         opcao2 = menuCalculo();
 
                         if (opcao2 < 0 || opcao2 > 3) {
-                            System.out.println("Opção invalida tente novamente!!!!!!!");
+                            System.out.println("Opção Invalida!");
                         }
+
                         switch (opcao2) {
                             case 1: {
-                                System.out.println("Perimetro" + quadrado.calculaPerimetro());
+                                System.out.println("Perimetro: " + forma.calculaPerimetro());
                                 break;
-
                             }
                             case 2: {
-                                System.out.println("Area: " + quadrado.calculaArea());
+                                System.out.println("Area: " + forma.calculaArea());
                                 break;
                             }
                             case 3: {
-                                System.out.println("Volume:" + quadrado.calculaVolume());
+                                System.out.println("Volume: " + forma.calculaVolume());
                                 break;
                             }
+                        }
+                    } while (opcao2 != 0);
 
+                    break;
+                }
+                case 2: {
+
+                    System.out.println("Qual a cor do retangulo: ");
+                    String cor = sc.next();
+                    System.out.println("Qual a largura do retangulo: ");
+                    double largura = sc.nextDouble();
+                    System.out.println("Qual a altura do retangulo: ");
+                    double altura = sc.nextDouble();
+                    System.out.println("Qual o comprimento do retangulo: ");
+                    double comprimento = sc.nextDouble();
+
+                    forma = new Retangulo(cor, largura, altura, comprimento);
+
+                    int opcao2;
+                    do {
+                        opcao2 = menuCalculo();
+
+                        if (opcao2 < 0 || opcao2 > 3) {
+                            System.out.println("Opção Invalida!");
                         }
 
-                    }while (opcao2 != 0) ;
+                        switch (opcao2) {
+                            case 1: {
+                                System.out.println("Perimetro: " + forma.calculaPerimetro());
+                                break;
+                            }
+                            case 2: {
+                                System.out.println("Area: " + forma.calculaArea());
+                                break;
+                            }
+                            case 3: {
+                                System.out.println("Volume: " + forma.calculaVolume());
+                                break;
+                            }
+                        }
+                    } while (opcao2 != 0);
 
-                        break;
-
-                    }
+                    break;
                 }
+            }
+        } while (opcao != 0);
+    }
 
-            } while (opcao != 0) ;
-        }
-        static public int menuCalculo () {
-            System.out.println("Qual calculo deseja fazer");
-            System.out.println("Opções: \n " + "0.Voltar: \n" + "1.Perimetro: \n" + "2.Area: \n" + "3.Volume \n");
-            return sc.nextInt();
+    static public int menuCalculo() {
+        System.out.println("Qual calculo deseja fazer?");
+        System.out.println("Opções: \n"
+                + "0. Voltar \n"
+                + "1. Perimetro\n"
+                + "2. Area\n"
+                + "3. Volume\n");
+        return sc.nextInt();
+    }
 
+    static public void retornaCalculo(int opcao2, Formageometrica forma) {
+        switch (opcao2) {
+            case 1: {
+                System.out.println("Perimetro: " + forma.calculaPerimetro());
+                break;
+            }
+            case 2: {
+                System.out.println("Area: " + forma.calculaArea());
+                break;
+            }
+            case 3: {
+                System.out.println("Volume: " + forma.calculaVolume());
+                break;
+            }
         }
     }
+}
+
+
 
